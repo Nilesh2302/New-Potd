427ff387b4d13a675f8dadd9d845ea1463727612
Nilesh2302 <118660370+Nilesh2302@users.noreply.github.com>
2024-01-08 19:57:05 +0530

    Time: 85 ms (84.24%), Space: 14.5 MB (9.76%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -2,26 +2,31 @@ class Solution(object):
     def setZeroes(self, arr):
         
         #t.c : O(mn) and s.c. is O(m+n)
+        
         m = len(arr)
         n = len(arr[0])
+        
         r = set()
         c = set()
+        
         for i in range(m):
             for j in range(n):
                 if arr[i][j]==0:
                     r.add(i)
                     c.add(j)
         
+        #make row with 0
         for i in r:
             for j in range(n):
                 arr[i][j]=0
         
+        #make column with 0
         for i in range(m):
             for j in c:
                 arr[i][j]=0
         
         return arr
-
+        
 
 
           
