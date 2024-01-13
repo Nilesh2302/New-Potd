e49243036fd8a34e644f2015cc5e9d0e1611d7f2
Nilesh2302 <118660370+Nilesh2302@users.noreply.github.com>
2024-01-13 16:24:03 +0530

    Time: 152 ms (66.02%), Space: 24.9 MB (24.46%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -7,18 +7,21 @@ class Solution(object):
         """
         k = k%len(arr)
         arr.reverse()
-        s = 0
-        e = k-1
-        while s<=e:
-            arr[s],arr[e]=arr[e],arr[s]
-            s+=1
-            e-=1
-            
+       
         
-        s = k
-        e = len(arr)-1
+        s=0
+        e=k-1
+        
         while s<=e:
             arr[s],arr[e]=arr[e],arr[s]
             s+=1
             e-=1
-            
+        
+        s=k
+        e=len(arr)-1
+        while s<=e:
+            arr[s],arr[e]=arr[e],arr[s]
+            s+=1
+            e-=1
+        
+        return reversed(arr)
