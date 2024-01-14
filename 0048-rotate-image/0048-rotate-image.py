02c47e5333110e733cd047303aedbfde46e2ba74
Nilesh2302 <118660370+Nilesh2302@users.noreply.github.com>
2024-01-14 12:35:12 +0530

    Time: 13 ms (87.25%), Space: 13.3 MB (69.58%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.py
+++ 0048-rotate-image/0048-rotate-image.py
@@ -1,19 +1,14 @@
 class Solution(object):
-    def rotate(self, arr):
-        r = len(arr)
-        c = len(arr[0])
-        
-        for i in range(r):
-            for j in range(i):
-                    arr[i][j],arr[j][i]=arr[j][i],arr[i][j]
-        
-        for i in arr:
-            i.reverse()
-        
-        
-        
+    def rotate(self, mat):
         """
         :type matrix: List[List[int]]
         :rtype: None Do not return anything, modify matrix in-place instead.
         """
         
+        for i in range(len(mat)):
+            for j in range(i):
+                mat[i][j],mat[j][i]=mat[j][i],mat[i][j]
+        
+        for i in mat:
+            i.reverse()
+            
